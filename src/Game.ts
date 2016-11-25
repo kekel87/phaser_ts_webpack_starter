cbbd8ccf92141202ba1629269e4e8a306079feb6
kekel87 <michael.parry.87@gmail.com>
2016-11-25 11:49:37 +0100

    MAJ des dépendances

--- src/Game.ts
+++ src/Game.ts
@@ -1,3 +1,5 @@
+/// <reference path="../node_modules/phaser/typescript/phaser.comments.d.ts" />
+
 import Boot from './states/Boot';
 import Loader from './states/Loader';
 import Main from './states/Main';
